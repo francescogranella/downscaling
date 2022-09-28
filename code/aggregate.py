d166d061e83f17885d2a6ff3ce8f1a0528f214e7
Francesco Granella <francesco.granella@phd.unibocconi.it>
2022-09-28 16:49:33 +0200

    select only certain models
    + Select non-hot models following Hausfather et al (2022)
    + Select 'historical', 'ssp585', 'ssp370', 'ssp245', 'ssp126'
    + Select only model variant _r1i1p1f1_
    + Select only models with monthly data [for the moment]. Daily data is computationally heavy

--- code/aggregate.py
+++ code/aggregate.py
@@ -13,6 +13,7 @@ from dask.diagnostics import ProgressBar
 from rasterstats import zonal_stats
 from shapely.geometry import Polygon
 from tqdm import tqdm
+from xmip.preprocessing import combined_preprocessing
 
 import context
 from functions import agg_on_model, agg_on_year, agg_on_space
@@ -31,7 +32,7 @@ def vectorize_raster(ds: xr.Dataset) -> gpd.GeoDataFrame:
     # Make a vector from the CMIP6 raster
     df = ds[['y', 'x']].to_dataframe().reset_index()
     polygons = []
-    for i, row in tqdm(df.iterrows(), total=len(df)):
+    for i, row in df.iterrows():
         lat = row[_lat]
         lon = row[_lon]
         polygons.append(Polygon([(lon - w, lat - h), (lon + w, lat - h), (lon + w, lat + h), (lon - w, lat + h)]))
@@ -71,8 +72,6 @@ assert borders.iso3.nunique() == len(borders)
 path = context.projectpath() + r"/data/in/population/gpw-v4-population-count-rev11_2000_2pt5_min_tif/gpw_v4_population_count_rev11_2000_2pt5_min.tif"
 population = prepare_pop(path)
 
-# %% For each file, extract the grid (could vary across CMIP6 files), compute and attach population weights to it.
-
 # %% Download from Pangeo
 url = "https://storage.googleapis.com/cmip6/pangeo-cmip6.json"
 col = intake.open_esm_datastore(url)
@@ -90,22 +89,31 @@ else:
     df = pd.read_csv('https://storage.googleapis.com/cmip6/pangeo-cmip6.csv')
     df.to_csv(path)
 
-df = df[df.activity_id == 'CMIP']
 source_ids = df.source_id.unique()
 
+# Selecting non-hot models following Hausfather et al Nature 2022 https://doi.org/10.1038/d41586-022-01192-2
+_models = pd.read_excel(context.projectpath() + '/data/in/cmip6/Hausfather et al Nature 2022 supporting data.xlsx',
+                        skiprows=1)
+_models = _models.iloc[:58]
+screened_models = _models.loc[_models['TCR Screen (likely) 1.4-2.2º'] == 'Y', 'Model Name'].unique()
+
+# Assert all screened models are in Pangeo
+assert bool(set(screened_models) & set(source_ids))
+
 # load multiple models at once
-query = dict(activity_id=['CMIP'],
-             experiment_id=['historical'],
-             table_id=['Amon'],
-             source_id=source_ids,  # ['CMCC-CM2-HR4'],
-             variable_id=['tas'],
-             grid_label=['gn'],
-             member_id=['r1i1p1f1']
-             )
+experiments = ['historical']  #, 'ssp585', 'ssp370', 'ssp245', 'ssp126']
+query = dict(
+    # activity_id=['CMIP'],
+    experiment_id=experiments,
+    table_id=['Amon'],
+    source_id=screened_models,
+    variable_id=['tas'],
+    grid_label=['gn', 'gr', 'gr1'],
+    member_id=['r1i1p1f1']
+)
 # possible values of table_id with description  https://clipc-services.ceda.ac.uk/dreq/index/miptable.html
 # possible values of variable_id with description https://clipc-services.ceda.ac.uk/dreq/index/var.html
 cat = col.search(**query)
-from xmip.preprocessing import combined_preprocessing
 
 datasets_dict = cat.to_dataset_dict(
     zarr_kwargs={'consolidated': True, 'decode_times': True})  # , preprocess=combined_preprocessing)
@@ -114,10 +122,13 @@ datasets_dict = cat.to_dataset_dict(
 # for path in paths:
 #     print(path.stem)
 #     ds = xr.open_dataset(path)
-for name, ds in tqdm(datasets_dict.items()):
+
+# %% Process
+pbar = tqdm(datasets_dict.items())
+for name, ds in pbar:
+    pbar.set_description(name)
     folder = context.projectpath() + f'/data/out/cmip/{name}'
     Path(folder).mkdir(parents=True, exist_ok=True)
-
     try:
         ds = combined_preprocessing(ds)
     except:
@@ -125,8 +136,16 @@ for name, ds in tqdm(datasets_dict.items()):
     # deal with pressure levels
     if 'plev' in ds.coords:
         ds = ds.sel(plev=ds.plev.max())
-    ds = ds.reset_coords(drop=True)  # clean up
-    # ds = ds.sel(time=slice('1960-01-01', '2019-12-01'))
+    # drop member_id if only one dimension
+    if len(list(ds.member_id)) == 1:
+        ds = ds.drop_vars('member_id')
+
+    # # Subset for testing purposes
+    # ds = ds.sel(time=slice('2014-01-01', '2014-12-31'))
+    # ds = ds.rio.write_crs('EPSG:4326')
+    # ds = ds.rio.clip_box(minx=0,maxx=20,miny=30,maxy=60)
+    # ds = ds.reset_coords(drop=True)  # clean up
+
     # Fix longitude from (0,360) to (-180,180). Has to be before clipping
     ds = ds.assign_coords({"x": (((ds.x + 180) % 360) - 180)})
     ds = ds.sortby('x')
@@ -134,8 +153,6 @@ for name, ds in tqdm(datasets_dict.items()):
     # Clip to borders of countries [saves memory]
     ds = ds.rio.write_crs('EPSG:4326')
     ds = ds.rio.clip(borders.geometry.values, borders.crs, all_touched=True, drop=True)
-    # # Subset for testing purposes
-    # ds = ds.rio.clip_box(minx=-10,maxx=20,miny=0,maxy=60)
     # Prepare vector
     grid = vectorize_raster(ds)
     # Split the grid where it crosses borders. Each cell is split into polygons, each belonging to 1 country
@@ -165,6 +182,11 @@ for name, ds in tqdm(datasets_dict.items()):
     global_mean = global_mean.to_dataframe()
     for var in variables:
         global_mean[var].reset_index().to_parquet(folder + f'/{var}_global_mean.parq')
+    try:
+        ds = agg_on_model(ds, func='mean')
+    except ValueError:
+        # If member_id is unique and has been dropped
+        pass
     ds = agg_on_space(ds, func='weighted_mean', weight=ds.population)
     ds = agg_on_year(ds, func='mean')
 
@@ -172,7 +194,7 @@ for name, ds in tqdm(datasets_dict.items()):
     ds = ds.drop('population')
     ds = ds.reset_coords(drop=True)  # clean up
 
-    # Exporting to dataframe is very..
+    # Exporting to dataframe is very slow
     with ProgressBar():
         ds = ds.compute()
     df = ds.to_dataframe()
@@ -180,26 +202,29 @@ for name, ds in tqdm(datasets_dict.items()):
         df[var].reset_index().to_parquet(folder + f'/{var}.parq')
 
 # %% Combine results
+cmip_path = Path(context.projectpath() + '/data/out/cmip')
 # Surface air temperature
-files = list(Path(context.projectpath() + '/data/out/cmip').glob('*/tas.parq'))
+files = list(cmip_path.glob('*/tas.parq'))
 l = []
 for file in files:
     _df = pd.read_parquet(file)
-    _df['model'] = file.parent.stem
+    _df['model'] = '.'.join([file.parent.stem.split('.')[i] for i in [1, 2, 4]])
+    _df['scenario'] = file.parent.stem.split('.')[3]
     l.append(_df)
 df = pd.concat(l)
 
 # Global mean of Surface air temperature
-files = list(Path(context.projectpath() + '/data/out/cmip').glob('*/tas_global_mean.parq'))
+files = list(cmip_path.glob('*/tas_global_mean.parq'))
 l = []
 for file in files:
     _df = pd.read_parquet(file)
-    _df['model'] = file.parent.stem
+    _df['model'] = '.'.join([file.parent.stem.split('.')[i] for i in [1, 2, 4]])
+    _df['scenario'] = file.parent.stem.split('.')[3]
     l.append(_df)
 global_mean = pd.concat(l)
 global_mean.rename(columns={'tas': 'avgtas'}, inplace=True)
 
-df = pd.merge(df, global_mean, on=['year', 'model'], how='outer')
+df = pd.merge(df, global_mean, on=['year', 'model', 'scenario'], how='outer')
 
 # Convert Kelvin to Celsius
 try:
@@ -212,18 +237,26 @@ df.to_parquet(context.projectpath() + '/data/out/data.parq')
 df.to_csv(context.projectpath() + '/data/out/data.csv', index=False)
 
 # %% Estimate coefficients with OLS
+df = pd.read_parquet(context.projectpath() + '/data/out/data.parq')
 
+# remove avg GMT 1850-1899
+df['avgtas'] -= df[df.year.between(1850, 1899)].groupby(['model']).avgtas.mean().values
+
+# estimate
 l = []
 for (iso3, model), g in tqdm(df.groupby(['iso3', 'model'])):
-    try:
-        res = smf.ols(formula='tas ~ avgtas', data=g).fit()
-    except ValueError:
-        continue
-    _ = pd.DataFrame({'iso3': [iso3], 'model': [model],
-                      'a': res.params['Intercept'], 'b': res.params['avgtas'],
-                      'a_se': res.HC3_se['Intercept'], 'b_se': res.HC3_se['avgtas'],
-                      })
-    l.append(_)
+    for ssp in [x for x in df.scenario.unique() if 'ssp' in x]:
+        dat = g[g.scenario.isin(['historical', ssp])]
+        try:
+            res = smf.ols(formula='tas ~ avgtas', data=dat).fit()
+        except ValueError:
+            continue
+        _ = pd.DataFrame({'iso3': [iso3], 'model': [model], 'scenario': ['historical_' + ssp],
+                          'a': res.params['Intercept'], 'b': res.params['avgtas'],
+                          'a_se': res.HC3_se['Intercept'], 'b_se': res.HC3_se['avgtas'],
+                          'r2': res.rsquared
+                          })
+        l.append(_)
 coefs = pd.concat(l).reset_index(drop=True)
 coefs.to_parquet(context.projectpath() + '/data/out/coefficients.parq')
 coefs.to_csv(context.projectpath() + '/data/out/coefficients.csv', index=False)
